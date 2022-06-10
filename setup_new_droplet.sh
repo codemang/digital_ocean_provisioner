d48e9f29def47218f8708be5227c7fa7f42d94a5
Nathaniel Rubin <nateboop@gmail.com>
2022-06-10 18:48:26 -0400

    Add a comment linking to the instructional guide that was followed.

--- setup_new_droplet.sh
+++ setup_new_droplet.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# This script sets up a new DigitalOcean droplet according to the instructions found here:
+# https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-22-04
+
 if [[ $# -eq 0 ]]; then
   echo "ERROR: You must pass in the username of the new user account you'd like to create on the droplet. Exiting."
   exit 1
 fi
 
 username=$1
+
 echo "Setting up a new droplet for user: $username"
 
 echo "Adding a new user with no password and give them sudo access."
