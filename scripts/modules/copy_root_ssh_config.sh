4f3fb96c64f3802e6799995ccee104a8decd2c18
Nathaniel Rubin <nateboop@gmail.com>
2023-06-29 22:53:11 -0400

    Add script for setting up a digital ocean droplet

--- scripts/modules/copy_root_ssh_config.sh
+++ scripts/modules/copy_root_ssh_config.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -lt 1 ]; then
+  echo "ERROR: You must pass in the username of the new user account you'd like to create. Exiting."
+  exit 1
+fi
+
+username=$1
+
 echo "Copying the SSH config from the root user to the new user, so that you can SSH in as the new user."
 rsync --archive --chown=$username:$username ~/.ssh /home/$username
 
---
+++ scripts/setup_digital_ocean_droplet.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "ERROR: You must pass in the username of the new user account you'd like to create on the droplet, and the email you'd like to use for nginx. Exiting."
+  exit 1
+fi
+
+username=$1
+email=$2
+
+# https://unix.stackexchange.com/a/273348
+script_dir="${0%/*}"
+
+sudo ./$script_dir/modules/add_primary_user.sh $username
+sudo ./$script_dir/modules/copy_root_ssh_config.sh $username
+sudo ./$script_dir/modules/setup_firewall.sh
+sudo ./$script_dir/modules/add_swap_memory.sh
+su $username -c ./$script_dir/modules/setup_docker.sh
+su $username -c ./$script_dir/modules/setup_github_ssh_key.sh
+su $username -c "./$script_dir/modules/setup_nginx_proxy.sh $email"
+su $username -c ./$script_dir/modules/setup_app_deployer.sh
+su $username -c ./$script_dir/modules/enable_digital_ocean_metrics.sh
+
+echo "All done! Rebooting so new configs can take effect."
+sudo reboot
