f2f5c4a2a7231a58525c1083a364f35256626a9d
Nathaniel Rubin <nateboop@gmail.com>
2024-11-20 20:57:40 -0500

    Add vim installation steps

--- scripts/modules/setup_system_packages.sh
+++ scripts/modules/setup_system_packages.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-sudo apt-get install -y fzf
-sudo apt-get install -y ripgrep
+sudo apt upgrade -y
+sudo apt install -y fzf
+sudo apt install -y ripgrep
+
+# Install Vim
+
+# https://github.com/neovim/neovim/wiki/Building-Neovim#ubuntu--debian
+sudo apt install -y ninja-build gettext cmake unzip curl
+
+# https://github.com/neovim/neovim/wiki/Building-Neovim#quick-start
+git clone https://github.com/neovim/neovim
+cd neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+
+# Install nvchad
+# https://nvchad.com/docs/quickstart/install
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
